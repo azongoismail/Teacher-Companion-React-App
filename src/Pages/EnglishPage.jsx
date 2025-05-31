d17f64930895f5643bf232c1a8375dd6d712d15f
Azongo Ismail <ismailibnazongo@gmail.com>
2025-05-31 14:02:18 +0000

    deleted pdf file

--- src/Pages/EnglishPage.jsx
+++ src/Pages/EnglishPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import LessonPlan from '../components/LessonPlan'
-import Image from '/Downloads/project.pdf';
+// import Image from '/Downloads/project.pdf';
 const EnglishPage = () => {
   return (
     <div className='flex flex-1 fex-row gap-4 flex-wrap mx-auto lg:w-[60%]'>
-      <LessonPlan  subject="English Lang." topic="Nouns" src={Image}/>
+      <LessonPlan  subject="English Lang." topic="Nouns" />
       <LessonPlan />
       <LessonPlan />
       <LessonPlan />
