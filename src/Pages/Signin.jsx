1a92a7c50132fa98c2976424e6076d849a83fbe1
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-22 11:40:35 +0000

    added sign in page

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Signin = () => {
   return (
-    <div>
+    <div className='flex min-h-[70vh] justify-center'>
         <form >
             <h1>Sign in</h1>
-            <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
-            <div>
-                <input placeholder='name' type="text" />
-                <input placeholder="email" type="email" />
-                <input placeholder=" password" type="password" />
-                <button type="submit"><Link to='/'>Sign in</Link></button>
+            <p> Don't have an account? <Link to='/signup'>Sign up</Link></p>
+            <div className='flex flex-col max-w-full'>
+                <input className=' bg-white py-4 px-3 mt-4 ' placeholder="email" type="email" />
+                <input className=' bg-white py-4 px-3 mt-4 ' placeholder=" password" type="password" />
+                <button className=' bg-blue-600 py-4 px-3 mt-4 text-white ' type="submit"><Link to='/'>Sign in</Link></button>
             </div>
         </form>
     </div>
