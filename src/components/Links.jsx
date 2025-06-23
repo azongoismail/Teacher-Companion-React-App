55391ef2d81820f5f6cc7bb92c5b086a5c81f2c2
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-23 18:27:22 +0000

    changed font-size from 2xl to xl

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,10 +5,10 @@ const Links = () => {
   return (
    <>
    <div>
-    <p> <Link to='/' className='cursor-pointer text-white text-2xl'>Home</Link></p>
-    <p> <Link to='basics'className='cursor-pointer text-white text-2xl'>Basics</Link></p>
-    <p> <Link to='lesson plan'className='cursor-pointer text-white text-2xl'>Lesson Plan</Link></p>
-    <p> <Link to='contact'className='cursor-pointer text-white text-2xl'>Contact</Link></p>
+    <p> <Link to='/' className='cursor-pointer text-white text-xl'>Home</Link></p>
+    <p> <Link to='basics'className='cursor-pointer text-white text-xl'>Basics</Link></p>
+    <p> <Link to='lesson plan'className='cursor-pointer text-white text-xl'>Lesson Plan</Link></p>
+    <p> <Link to='contact'className='cursor-pointer text-white text-xl'>Contact</Link></p>
     
    </div>
    </>
