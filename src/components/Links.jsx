d39331c856347de68d420af7e65d75e066ffa6e0
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-25 17:46:24 +0000

    changed font-sizes from xl to lg

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,10 +5,10 @@ const Links = () => {
   return (
    <>
    <div>
-    <p> <Link to='/' className='cursor-pointer text-white text-xl'>Home</Link></p>
-    <p> <Link to='basics'className='cursor-pointer text-white text-xl'>Basics</Link></p>
-    <p> <Link to='lesson plan'className='cursor-pointer text-white text-xl'>Lesson Plan</Link></p>
-    <p> <Link to='contact'className='cursor-pointer text-white text-xl'>Contact</Link></p>
+    <p> <Link to='/' className='cursor-pointer text-white text-lg'>Home</Link></p>
+    <p> <Link to='basics'className='cursor-pointer text-white text-lg'>Basics</Link></p>
+    <p> <Link to='lesson plan'className='cursor-pointer text-white text-lg'>Lesson Plan</Link></p>
+    <p> <Link to='contact'className='cursor-pointer text-white text-lg'>Contact</Link></p>
     
    </div>
    </>
