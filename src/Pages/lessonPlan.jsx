c0f7fff80b0ade9dd24643c582f70676d4bca33b
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-26 17:36:08 +0000

    changed styles

--- src/Pages/lessonPlan.jsx
+++ src/Pages/lessonPlan.jsx
@@ -7,28 +7,13 @@ const lessonPlan = () => {
     <div className='flex flex-1 flex-col items-center gap-3 mx-auto sm:justify-center md:flex-row md:flex-wrap md:w-[60%] lg:w-[60%] '>
 
         <LessonPlanCard subject={'Science'} src="/SciencePage" />
-      
-      
         <LessonPlanCard subject={'Maths'} src="/mathPage" />
-     
-      
         <LessonPlanCard subject={'English Lang.'} src="/EnglishPage" />
-      
-      
         <LessonPlanCard subject={'Social Studies'} src="/SocialStudiesPage" />
-      
-     
         <LessonPlanCard subject={'RME'} src="/RMEpage"/>
-     
-      
         <LessonPlanCard subject={'CAD'} src="/CADpage" />
-      
-     
         <LessonPlanCard subject={'Career tech.'} src="/CarrerTechPage" />
-    
-      
         <LessonPlanCard subject={'Computing'} src="Computing" />
-   
     </div>
   );
 };
--- src/components/LessonPlanCard.jsx
+++ src/components/LessonPlanCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {useNavigate, Link} from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 const LessonPlanCard = ({subject, src}) => {
-  const navigate = useNavigate();
+ 
 
   return (
    <>
-   <div className='bg-[#fff] border-[#fff] shadow-lg w-[70vw] h-[30vh] rounded-lg ml-6 my-6 text-center drop-shadow-xl flex flex-col items-center justify-start text-center sm:w-2/3 lg:w-[40%] lg:h-[40vh]' >
+   <div className='bg-[#fff] border-[#fff] shadow-lg w-[50vw] h-[40vh] rounded-lg ml-6 my-6 text-center  flex flex-col items-center justify-start text-center sm:w-2/3 lg:w-[20%] lg:h-[40vh]' >
    <div className='bg-orange-600 w-[100%] h-[50%] rounded-t-lg'>
 
    </div>
-    <h2 className='text-lg font-bold color-silver'><Link to={src}>{subject}</Link></h2>
+    <h2 className='text-xl font-open-sans color-red-600'><Link to={src}>{subject}</Link></h2>
    </div>
    </>
   )
