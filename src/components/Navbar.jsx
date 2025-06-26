6285f3e2d57c3b431ce86be307c6b072063e6a55
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-26 09:29:56 +0000

    changed styles on navbar and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   console.log(user);
   return (
     <>
-    <nav className=' bg-blue-600 flex  items-center justify-between  p-4 w-[100vw] h-[15vh] sm:flex '>
+    <nav className=' bg-blue-600 flex  items-center justify-between  p-4 w-[100vw] h-[25vh] sm:flex '>
      <Link to='/' className=' hidden sm:block  '><img src={Frame} alt="" className='w-2xl' /></Link> 
      <button className=' block mx-[100px] sm:hidden cursor-pointer m-6 sm:absolute' onClick={ ()=>setShowMenu(!showMenu)}>
     {showMenu ? <FaAlignJustify className='size-10 absolute right-5 top-5'/> : <FaX className='size-10 mr-3 absolute right-5 top-5'/>}
@@ -22,27 +22,27 @@ const Navbar = () => {
       <div className={`${showMenu ? 'hidden' : 'block mr-10'} sm:block sm:flex sm:items-center `} >
               <ul className='sm:flex sm:flex-row sm:space-x-5'>
                   <li >
-                      <Link to="/" className='text-[20px] text-white font-medium sm:text-lg'>Home</Link>
+                      <Link to="/" className='text-base text-white font-medium sm:text-lg'>Home</Link>
                     
                   </li>
                   <li>
-                      <Link to="/basics" className='text-[20px] text-white font-medium sm:text-lg'>Basics</Link>
+                      <Link to="/basics" className='text-base text-white font-medium sm:text-lg'>Basics</Link>
                     
                   </li>
                   <li>
-                      <Link to="/lesson plan" className='text-[20px] text-white font-medium sm:text-lg'>Lesson Plan</Link>
+                      <Link to="/lesson plan" className='text-base text-white font-medium sm:text-lg'>Lesson Plan</Link>
                       
                   </li>
                   <li>
-                      <Link to="/contact" className='text-[20px] text-white font-medium sm:text-lg'>Contact</Link>
+                      <Link to="/contact" className='text-base text-white font-medium sm:text-lg'>Contact</Link>
                       
                   </li>
                   <li>
-                      <Link to="/signup" className='text-[20px] text-white font-medium border inline-block py-2 px-4 sm:text-lg '>Sign up</Link>
+                      <Link to="/signup" className=' bg-red-200 text-base color-black text-white font-medium  inline-block py-2 px-4 sm:text-base '>Sign up</Link>
                       
                   </li>
                  {
-                    user && (<p onClick={signOut} className='bg-red-200 inline-block border-red-600 p-4 cursor-pointer'>Sign Out</p>)
+                    user && (<p onClick={signOut} className=' inline-block border px-4 py-2 cursor-pointer text-base mt-4 sm:mt-0 md:mt-0 '>Sign Out</p>)
                  } 
               </ul>
       </div>   
