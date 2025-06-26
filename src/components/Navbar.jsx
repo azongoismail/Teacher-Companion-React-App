d81ea3392d8d16537150ccb511c01e1573196b97
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-26 09:51:29 +0000

    font sizes of links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   console.log(user);
   return (
     <>
-    <nav className=' bg-blue-600 flex  items-center justify-between  p-4 w-[100vw] h-[25vh] sm:flex '>
+    <nav className=' bg-blue-600 flex  items-center justify-between  p-4 w-[100vw] h-[25vh] sm:flex sm:h-[15vh]'>
      <Link to='/' className=' hidden sm:block  '><img src={Frame} alt="" className='w-2xl' /></Link> 
      <button className=' block mx-[100px] sm:hidden cursor-pointer m-6 sm:absolute' onClick={ ()=>setShowMenu(!showMenu)}>
     {showMenu ? <FaAlignJustify className='size-10 absolute right-5 top-5'/> : <FaX className='size-10 mr-3 absolute right-5 top-5'/>}
@@ -22,19 +22,19 @@ const Navbar = () => {
       <div className={`${showMenu ? 'hidden' : 'block mr-10'} sm:block sm:flex sm:items-center `} >
               <ul className='sm:flex sm:flex-row sm:space-x-5'>
                   <li >
-                      <Link to="/" className='text-base text-white font-medium sm:text-lg'>Home</Link>
+                      <Link to="/" className='text-base text-white font-medium sm:text-base '>Home</Link>
                     
                   </li>
                   <li>
-                      <Link to="/basics" className='text-base text-white font-medium sm:text-lg'>Basics</Link>
+                      <Link to="/basics" className='text-base text-white font-medium sm:text-base'>Basics</Link>
                     
                   </li>
                   <li>
-                      <Link to="/lesson plan" className='text-base text-white font-medium sm:text-lg'>Lesson Plan</Link>
+                      <Link to="/lesson plan" className='text-base text-white font-medium sm:text-base'>Lesson Plan</Link>
                       
                   </li>
                   <li>
-                      <Link to="/contact" className='text-base text-white font-medium sm:text-lg'>Contact</Link>
+                      <Link to="/contact" className='text-base text-white font-medium sm:text-base'>Contact</Link>
                       
                   </li>
                   <li>
