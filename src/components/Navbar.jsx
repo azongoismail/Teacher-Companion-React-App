8c41ff1a1ed4cf6085054af925a0347a63ca83f3
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-26 16:18:23 +0000

    changed thebackground color  sign up button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
                       
                   </li>
                   <li>
-                      <Link to="/signup" className=' bg-red-200 text-base color-black text-white font-medium  inline-block py-2 px-4 sm:text-base '>Sign up</Link>
+                      <Link to="/signup" className=' bg-red-600 text-base color-black text-white font-medium  inline-block py-2 px-4 sm:text-base '>Sign up</Link>
                       
                   </li>
                  {
